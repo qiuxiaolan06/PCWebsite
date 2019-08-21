586f55bda297075ff42f22a6eae24a6bc6b5c0e7
QiuYuan <qiuyuan@milanosoft.com>
2019-08-21 17:08:54 +0800

    A:个人中心页面-我的中心

--- src/views/shoppingMall/user/usercenter.vue
+++ src/views/shoppingMall/user/usercenter.vue
@@ -25,9 +25,9 @@
                     <div class="trading base">
                         <ul>
                             <li>我的交易</li>
-                            <li>我的购物车</li>
-                            <li>我的订单</li>
-                            <li>评价晒单</li>
+                            <li @click="cur=0" :class="{active:cur==0}">我的购物车</li>
+                            <li @click="cur=1" :class="{active:cur==1}">我的订单</li>
+                            <li @click="cur=2" :class="{active:cur==2}">评价晒单</li>
                         </ul>
                     </div>
 
@@ -35,8 +35,8 @@
                     <div class="personal base">
                         <ul>
                             <li>个人中心</li>
-                            <li>我的中心</li>
-                            <li>地址管理</li>
+                            <li @click="cur=3" :class="{active:cur==3}">我的中心</li>
+                            <li @click="cur=4" :class="{active:cur==4}">地址管理</li>
                         </ul>
                     </div>
 
@@ -44,13 +44,72 @@
                     <div class="account base">
                         <ul>
                             <li>账户管理</li>
-                            <li>个人信息</li>
-                            <li>修改密码</li>
+                            <li @click="cur=5" :class="{active:cur==5}">个人信息</li>
+                            <li @click="cur=6" :class="{active:cur==6}">修改密码</li>
                         </ul>
                     </div>
 
                 </div>
-                <div class="user_type">2</div>
+                <div class="user_type">
+                    <div v-show="cur==0">内容一</div>
+                    <div v-show="cur==1">内容二</div>
+                    <div v-show="cur==2">内容三</div>
+                    <div v-show="cur==3" class="mine_center">
+                        <div class="type_header">
+                            <div class="header_img">
+                                <img src="../../../assets/img/shoppingMall/detail/proDet.jpg" alt="">
+                            </div>
+                            <div class="type_info">
+                                <p>大橙子</p>
+                                <p>绑定邮箱:178****1506@qq.com</p>
+                                <p>修改资料>></p>
+                            </div>
+                        </div>
+
+                        <div class="type_box">
+                            <div class="box_one">
+                                <div class="box_bg">
+                                    <i class="el-icon-s-finance"></i>
+                                </div>
+
+                                <div class="box_font">
+                                    <p>待支付的订单：<span>10</span></p>
+                                    <p>查看待支付订单>></p>
+                                </div>
+                            </div>
+                            <div class="box_one">
+                                <div class="box_bg two">
+                                    <i class="el-icon-truck"></i>
+                                </div>
+                                <div class="box_font">
+                                    <p>待收货的订单：<span>10</span></p>
+                                    <p>查看待收货订单>></p>
+                                </div>
+                            </div>
+                            <div class="box_one">
+                                <div class="box_bg three">
+                                    <i class="el-icon-chat-dot-round"></i>
+                                </div>
+                                <div class="box_font">
+                                    <p>待评价的订单：<span>10</span></p>
+                                    <p>查看待评价订单>></p>
+                                </div>
+                            </div>
+                            <div class="box_one">
+                                <div class="box_bg four">
+                                    <i class="el-icon-star-off"></i>
+                                </div>
+                                <div class="box_font">
+                                    <p>喜欢的商品：<span>10</span></p>
+                                    <p>查看喜欢的商品>></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-show="cur==4">地址管理</div>
+                    <div v-show="cur==5">个人信息</div>
+                    <div v-show="cur==6">修改密码</div>
+                </div>
             </div>
 
         </div>
@@ -61,6 +120,12 @@
 <script>
     export default {
         name: "usercenter",
+        data(){
+            return {
+                tabPosition: 'left',
+                cur: 3 //默认选中我的中心
+            }
+        }
     }
 </script>
 
